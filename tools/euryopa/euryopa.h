969c1136c8b6cbd66ce62a3313c5e0f73350472b
Dryxio <momemtumcorpfzco@gmail.com>
2026-05-23 11:03:51 +0200

    sync

--- tools/euryopa/euryopa.h
+++ tools/euryopa/euryopa.h
@@ -438,7 +438,8 @@ void Redo(void);
 
 // Copy/Paste
 void CopySelected(void);
-void PasteClipboard(void);
+void CutSelected(void);
+int PasteClipboard(void);
 
 // Prefabs
 int ExportPrefab(const char *path);
